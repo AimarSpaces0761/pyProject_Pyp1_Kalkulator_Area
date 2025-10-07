fa7f956da934a717f6d72c592dd0c855fd0f1675
AimarSpaces <aimrikz.dilfa986@gmail.com>
2025-10-07 18:40:55 +0700

    kalkulator area

---
+++ main.py
@@ -0,0 +1,11 @@
+# kalkulator area di python
+# panjang, lebar dan tinggi
+
+panjang = float(input("Masukkan panjang dari kubus persegi panjang anda: "))
+lebar = float(input("Masukkan lebar dari kubus persegi panjang anda: "))
+tinggi = float(input("Masukkan tinggi dari kubus persegi panjang anda: "))
+
+vol = panjang * lebar * tinggi
+
+print()
+print(f"Volume yang anda masukkan adalah: {vol}cm^2")
